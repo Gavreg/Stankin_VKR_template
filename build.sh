4ab710993e8f3251fd7dd5b661e0b2695305a927
gavreg <faktorial5@gmail.com>
2026-07-31 17:07:22 +0300

    тестирую workflow + сборка в докере

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+export NPROC=$(( ( $(nproc || echo 4) ) * 2 ))
+#export NPROC=1
+
+
+export workdir="$PWD"
+
+
+compile_tex() {
+
+    set -o pipefail
+    set -e
+
+    f="$@"
+
+    texfile=$(basename "$f")
+    texfiledir=$(dirname "$f")
+    texfilename=$(basename "$texfile" .tex) 
+    
+    cd "$texfiledir"
+    
+    if docker run --rm -v "$workdir":/data vkrlateximage \
+        latexmk   -pdfxe  -interaction=nonstopmode  -recorder- -halt-on-error  -8bit --shell-escape -synctex=0 -cd "$f" #> /dev/null 2>&1 
+    then 
+        rm "$texfilename".log > /dev/null 2>&1 || true 
+        true
+    else
+        mv "$texfilename".log "$vkrlateximage".log.tmp  || true
+        docker run --rm -v "$workdir":/data vkrlateximage latexmk -C -cd "$f" > /dev/null 2>&1 
+        mv "$texfilename".log.tmp  "$texfilename".log  || true
+        exit 255
+    fi
+}
+export -f  compile_tex
+
+compile_tex main.tex
+
+#parallel  -j $NPROC --halt-on-error soon,fail=1 --verbose 'compile_tex {}' ::: \
+# main.tex #main_assigment.tex
+
---
+++ dockerfile
@@ -0,0 +1,5 @@
+FROM texlive/texlive:TL2025-historic
+RUN sed -i 's/main$/main contrib/' /etc/apt/sources.list && apt update && apt install -y fonts-liberation python3.14 ttf-mscorefonts-installer
+RUN fc-cache -f -v /usr/share/fonts
+WORKDIR /data
+
